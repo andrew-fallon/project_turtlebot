e3b6b206ca75d4033e1c5f3d4ed403b1ebab001f
TaneTatum <ttatum@stanford.edu>
2019-03-14 00:53:19 -0700

    Added apples, bananas, and brocoli, oh my!

--- scripts/store_locations.py
+++ scripts/store_locations.py
@@ -12,20 +12,32 @@ class Locations:
 
 	def __init__(self):
 		rospy.init_node('detected_locations', anonymous=True)
+		self.tf_listener = tf.TransformListener()
 		self.stop_pub = rospy.Publisher('stop_sign_markers', MarkerArray, queue_size=10)
 		self.bottle_pub = rospy.Publisher('bottle_markers', MarkerArray, queue_size=10)
-		self.tf_listener = tf.TransformListener()
+		self.apple_pub = rospy.Publisher('apple_markers', MarkerArray, queue_size=10)
+		self.banana_pub = rospy.Publisher('banana_markers', MarkerArray, queue_size=10)
+		self.brocoli_pub = rospy.Publisher('brocoli_markers', MarkerArray, queue_size=10)
 
 		self.detection_thresh = 0.5
-		self.known_names = ["stop_sign", "bottle"]
+		self.known_names = ["stop_sign", "bottle", "apple", "banana", "brocoli"]
 
 		self.stop_array = MarkerArray()
 		self.bottle_array = MarkerArray()
+		self.apple_array = MarkerArray()
+		self.banana_array = MarkerArray()
+		self.brocoli_array = MarkerArray()
 
 		self.stop_id = 0
 		self.bottle_id = 0
+		self.apple_id = 0
+		self.banana_id = 0
+		self.brocoli_id = 0
 		self.stop_color = ColorRGBA(1.0,0.0,0.0,1.0)
 		self.bottle_color = ColorRGBA(0.0,0.0,1.0,1.0)
+		self.apple_color = ColorRGBA(0.5,0.0,0.0,1.0)
+		self.banana_color = ColorRGBA(1.0,1.0,1.0,1.0)
+		self.brocoli_color = ColorRGBA(0.5,0.6,1.0,1.0)
 		rospy.Subscriber('/detector/objects', DetectedObjectList, self.detected_callback)
 
 	def buildMarkers(self, array, id_count, color):
@@ -62,10 +74,22 @@ class Locations:
 					self.stop_array, self.stop_id = self.buildMarkers(self.stop_array, self.stop_id, self.stop_color)
 				elif object_name == self.known_names[1]:
 					self.bottle_array, self.bottle_id = self.buildMarkers(self.bottle_array, self.bottle_id, self.bottle_color)
+				elif object_name == self.known_names[2]:
+					self.apple_array, self.apple_id = self.buildMarkers(self.apple_array, self.apple_id, self.apple_color)
+				elif object_name == self.known_names[3]:
+					self.banana_array, self.banana_id = self.buildMarkers(self.banana_array, self.banana_id, self.banana_color)
+				elif object_name == self.known_names[3]:
+					self.brocoli_array, self.banana_id = self.buildMarkers(self.brocoli_array, self.brocoli_id, self.brocoli_color)
+
+
 
 	def loop(self):
 		self.stop_pub.publish(self.stop_array)
 		self.bottle_pub.publish(self.bottle_array)
+		self.apple_pub.publish(self.apple_array)
+		self.banana_pub.publish(self.banana_array)
+		self.brocoli_pub.publish(self.brocoli_array)
+
 
 	def run(self):
 		rate = rospy.Rate(10) # 10 Hz
